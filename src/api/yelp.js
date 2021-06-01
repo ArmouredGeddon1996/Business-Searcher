e9b332304b3756f69dceb95c00223aa2e32fb650
ArmouredGeddon1996 <59177426+ArmouredGeddon1996@users.noreply.github.com>
2021-06-01 17:29:28 +0100

    Update yelp.js

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
 	baseURL: 'https://api.yelp.com/v3/businesses',
 	headers: {
-		Authorization: 'Bearer GFym_vRb73ZsePQeyuMUE98VjY1xHdGVo-J3GNxxgIF1AIHcgTQgfACzsxi0FWvOEcCSO9jvgcfAGMm6mWFVUQLMeHsc1n7yzrlc0FhqbYNIxCm-zXGzbDp2W2evYHYx',
+		Authorization: 'Bearer',
 	}
 });
